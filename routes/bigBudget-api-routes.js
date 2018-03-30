b41dd9e11b160e1148e8cc95181212969c19c913
rocsteady888 <rocsteady888@gmail.com>
2018-03-30 15:27:09 -0400

    changed association in bigbudget

--- routes/bigBudget-api-routes.js
+++ routes/bigBudget-api-routes.js
@@ -1,77 +1,35 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-// Requiring our models
+// Requiring our models and passport as we've configured it
 var db = require("../models");
-
-// Routes
-// =============================================================
+var passport = require("../config/passport");
+var middleware = require('../config/middleware/isAuthenticated');
 module.exports = function(app) {
+  // Using the passport.authenticate middleware with our local strategy.
+  // If the user has valid login credentials, send them to the members page.
+  // Otherwise the user will be sent an error
+  app.get("/api/testBigBudget", middleware, function(req,res){
+      
+    db.BigBudget.findAll({
+        where:{
+            PersonId: req.user.id
+        },
+        include:[{
+          model: db.Person
+        }]
+    }).then(function(person){
+      res.json(person)
+      console.log(person);
 
-  // GET route for getting all of the bigBudgets
-  app.get("/api/bigBudgets", function(req, res) {
-    var query = {};
-    if (req.query.author_id) {
-      query.AuthorId = req.query.author_id;
-    }
+    })
+  })
+
+  app.get("/api/bigBudget", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.BigBudget.findAll({
-      where: query,
-      include: [db.Author]
-    }).then(function(dbBigBudget) {
-      res.json(dbBigBudget);
-    });
-  });
-
-  // Get rotue for retrieving a single bigBudgets
-  app.get("/api/bigBudgets/:id", function(req, res) {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.BigBudget.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Author]
-    }).then(function(dbBigBudget) {
-      res.json(dbBigBudget);
-    });
-  });
-
-  // POST route for saving a new bigBudgets
-  app.post("/api/bigBudgets", function(req, res) {
-    db.BigBudget.create(req.body).then(function(dbBigBudget) {
-      res.json(dbBigBudget);
-    });
-  });
-
-  // DELETE route for deleting bigBudgets
-  app.delete("/api/bigBudgets/:id", function(req, res) {
-    db.BigBudget.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbBigBudget) {
-      res.json(dbBigBudget);
-    });
-  });
-
-  // PUT route for updating bigBudgets
-  app.put("/api/bigBudgets", function(req, res) {
-    db.BigBudget.update(
-      req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      }).then(function(dbBigBudget) {
-      res.json(dbBigBudget);
+    // In this case, just db.Post
+    db.Interest.findAll({
+    }).then(function(dbInterest) {
+      res.json(dbInterest);
+      console.log("api routes")
     });
   });
 };
