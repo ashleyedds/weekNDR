75e24625dd6d121e89515fdf45fe752c03098273
Joseph S. Quinn <31451791+JS-Quinn@users.noreply.github.com>
2018-03-30 22:03:16 -0400

    Revert "dashboard big budget and big budget into interests table"

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     var userId;
 
     var interests = [];
 
-    $("#budgetClick").on("click", function () {
-        setTimeout(function () {
-            $.get("/api/user_data").then(function (data) {
-                userId = data.id;
-                console.log(data.id);
-            });
-        }, 2000);
+    $("#budgetClick").on("click", function() {
+        setTimeout(function() {
+          $.get("/api/user_data").then(function(data) {
+            userId = data.id;
+            console.log(data.id);
+         });
+       }, 2000);
     });
 
+    
 
-
-    $("#interestsClick").on("click", function () {
+    $("#interestsClick").on("click", function(){
         if ($("#movie-btn").is(":checked")) {
-
+            
             var movieAmount = $("#movie_amount").val().trim()
-
+            
             var interest = {
                 "title": "Movies",
                 "estCost": movieAmount,
@@ -39,7 +39,7 @@ $(document).ready(function () {
         }
         if ($("#shopping-btn").is(":checked")) {
             var shopAmount = $("#shopping_amount").val().trim()
-
+            
             var interest = {
                 "title": "Shopping",
                 "estCost": shopAmount,
@@ -50,7 +50,7 @@ $(document).ready(function () {
         }
         if ($("#dinner-btn").is(":checked")) {
             var dinnerAmount = $("#dinner_amount").val().trim();
-
+            
             var interest = {
                 "title": "Eating Out",
                 "estCost": dinnerAmount,
@@ -83,50 +83,40 @@ $(document).ready(function () {
         console.log(interests);
     })
 
-    $("#bigClick").on("click", function () {
+    $("#bigClick").on("click", function() {
         if ($("#vacation-btn").is(":checked")) {
-            var vacationAmount = $("#big_amount").val().trim();
-
-            var interest = {
-                "title": "Vacation",
-                "estCost": vacationAmount,
-                "PersonId": userId
-            }
-            addInterests(interest)
+            interests.push({
+                "Title": "Vacation",
+                "EstCost": "50"
+            })
         }
+
         if ($("#car-btn").is(":checked")) {
-            var carAmount = $("#big_amount").val().trim();
-
-            var interest = {
-                "title": "Car",
-                "estCost": carAmount,
-                "PersonId": userId
-            }
-            addInterests(interest)
+            interests.push({
+                "Title": "Car",
+                "EstCost": "50"
+            })
         }
-        if ($("#ring-btn").is(":checked")) {
-            var bigAmount = $("#big_amount").val().trim();
 
-            var interest = {
-                "title": "BigAmt",
-                "estCost": bigAmount,
-                "PersonId": userId
-            }
-            addInterests(interest)
+        if ($("#ring-btn").is(":checked")) {
+            interests.push({
+                "Tilte": "Big Purchase",
+                "EstCost": "50"
+            })
         }
 
         var myInterests = interests;
         console.log(interests)
         // getInterests(myInterests)
         getInterestList();
-
+        
     })
 
     function addInterests(interests) {
-        $.post("/api/interests", interests, function () {
+        $.post("/api/interests", interests, function() {
             // getInterests(interests);
         })
-    }
+      }
 
 
 })
